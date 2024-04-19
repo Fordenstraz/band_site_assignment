f95431cc07b0cb440f39ceb113791a358e5fd05a
Corey Bickford <cb6@live.ca>
2024-04-19 14:10:07 -0700

    Create API script

---
+++ scripts/band-site-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+//API URL: https://unit-2-project-api-25c1595833b2.herokuapp.com/
+
+// AUTHENTICATION: To register with the API and get a key, make a GET request to /register.
+// You can do this with the browser and you only need to do it once. Store the key in a global variable in your website.
+// You must append ?api_key=<your_api_key_here> to each of your API request URLs (except for /register).
+
+class BandSiteApi {
+	constructor(apiKey) {
+		this.apiKey = apiKey;
+		this.baseUrl = `https://unit-2-project-api-25c1595833b2.herokuapp.com/`;
+	}
+
+	postComment(comment) {
+		await;
+	}
+
+	getComments() {
+		await;
+	}
+
+	getShows() {
+		await;
+	}
+}
